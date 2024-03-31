0baa3321ac9aafb501438f07365a478ab342b8de
Florian Stradner <florian.stradne@tset.com>
2024-03-31 15:47:25 +0200

    initial commit

---
+++ fe/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1 class="text-rose-600">Welcome to VueQuery and SSE (Server Side Events)</h1>
+  </main>
+</template>
